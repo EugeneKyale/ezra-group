5c4b1b3bce420416cd4e3a186ab6abfe1fc460a4
Eugene Kyale <eugene.kyale@gmail.com>
2022-09-25 18:05:36 +0300

    site maintenance mode

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import { Route, Routes } from "react-router-dom";
 /**
  * Internal Dependencies
  */
-import Home from "./pages/home";
-import About from "./pages/about";
-import Subsidiary from "./pages/subsidiary";
-import Projects from "./pages/projects";
-import Privacy from "./pages/privacy";
 import Four0Four from "./pages/404";
 
 import "./App.scss";
@@ -19,11 +14,6 @@ import "./App.scss";
 const App = () => {
 	return (
 		<Routes>
-			<Route exact path="/" element={ <Home /> } />
-			<Route exact path="/about" element={ <About /> } />
-			<Route exact path="/subsidiary/:slug/:id" element={ <Subsidiary /> } />
-			<Route exact path="/projects" element={ <Projects /> } />
-			<Route exact path="/privacy" element={ <Privacy /> } />
 			<Route path="*" element={ <Four0Four /> } />
 		</Routes>
 	);
