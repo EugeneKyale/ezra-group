1c4fea97eb4b45b13ca579689cb8559fe919a1e4
Eugene Kyale <eugene.kyale@gmail.com>
2022-09-25 23:29:20 +0300

    clean up project details

--- src/pages/project/project.js
+++ src/pages/project/project.js
@@ -25,7 +25,7 @@ const Project = () => {
 	useEffect( () => {
 		axiosInstance({
 			method: 'get',
-			url: `projects/${ projectId }?populate=coverImage,infoCards.icon`
+			url: `projects/${ projectId }?populate=coverImage,infoCards.icon,projectDetails`
 		}).then( result => {
 			setProjectDetails( result.data.data.attributes );
 		}).catch( error => {
@@ -48,13 +48,13 @@ const Project = () => {
 						backgroundImage={ coverImage?.data.attributes.url }
 					/>
 					<div className={ styles.project__inner }>
-						<div className={ styles.project__inner_left }>
+						<div className={ styles.project__inner_left + " wow fadeInUp" } data-wow-delay=".3s">
 							<ReactMarkdown>
 								{ excerpt }
 							</ReactMarkdown>
 						</div>
 
-						<div className={ styles.project__inner_right }>
+						<div className={ styles.project__inner_right + " wow fadeInUp" } data-wow-delay=".5s">
 							<div className={ styles.project__inner_right_card }>
 								{ infoCards?.map( ( card ) => (
 										<Stats
@@ -66,6 +66,18 @@ const Project = () => {
 									))
 								}
 							</div>
+						</div>
+					</div>
+
+					<div className={ styles.project__inner }>
+						<div className={ styles.project__inner_bottom }>
+							{ projectDetails.projectDetails?.map( ( detail ) => (
+								<div className={ styles.project__inner_bottom_inner + " wow fadeInUp" } data-wow-delay=".5s">
+									<ReactMarkdown>
+										{ detail.description }
+									</ReactMarkdown>
+								</div>
+							))}
 						</div>
 					</div>
 				</main>
