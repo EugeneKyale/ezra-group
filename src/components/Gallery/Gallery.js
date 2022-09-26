c295bfa1c49ad5b974785438e998ad1c0dea8d9d
Eugene Kyale <eugene.kyale@gmail.com>
2022-09-26 23:19:22 +0300

    skeleton gallery

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,20 @@
+/**
+ * External Dependencies
+ */
+import React from "react";
+
+/**
+ * Internal Dependencies
+ */
+import { cmsUrl } from "../../_helpers/utils";
+import styles from "./Gallery.module.scss";
+
+const Gallery = ( { icon, title, number } ) => {
+	return (
+		<div className={ styles.gallery }>
+			
+		</div>
+	);
+};
+
+export default Gallery;
