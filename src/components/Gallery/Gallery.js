a3b098e3c15feeb5d7a9716e0cfd55eeeed1667b
Eugene Kyale <eugene.kyale@gmail.com>
2022-10-02 23:22:54 +0300

    fetch image gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -13,99 +13,21 @@ import 'lightgallery/css/lg-thumbnail.css';
 /**
  * Internal Dependencies
  */
-import { axiosInstance } from "../../_helpers/utils";
-import styles from "./Gallery.module.scss";
+import Photo from "./Photo";
 
 const Gallery = ( { ids } ) => {
-    const [ galleryImageId, setGalleryImageId ] = useState( '' );
-	const [ galleryImageUrl, setGalleryImageUrl ] = useState( '' );
-
-    const fetchGalleryUrls = async () => {
-        ids && ids.map( ( id ) => (
-            setGalleryImageId( id )
-        ));
-        
-		await axiosInstance({
-			method: 'get',
-			url: `media/${ galleryImageId }`
-		}).then( res => {
-			setGalleryImageUrl( res.data.media_details.sizes.full.source_url );
-		});
-	}
-
-    useEffect( () => {
-		fetchGalleryUrls();
-        console.log( galleryImageId );
-        console.log( galleryImageUrl );
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ galleryImageId ]);
 
 	return (
-		<div className={ styles.gallery }>
-            <LightGallery
-                className={ styles.gallery__inner }
-                speed={ 500 }
-                plugins={ [ lgThumbnail, lgZoom ] }
-            >
-                <a href="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80" 
-                    />
-                </a>
-                <a href="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80">
-                    <img
-                        className={ styles.gallery__inner_photo }
-                        alt=""
-                        src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" 
-                    />
-                </a>
-            </LightGallery>
-		</div>
+        <LightGallery
+            speed={ 500 }
+            plugins={ [ lgThumbnail, lgZoom ] }
+        >
+            { ids &&
+                ids.map( ( id ) => (
+                    <Photo id={ id } />
+                ))
+            }
+        </LightGallery>
 	);
 };
 
