16a749c59195b9b2cb187a76d2252356501e13d6
Eugene Kyale <eugene.kyale@gmail.com>
2022-10-05 16:01:54 +0300

    initial news page

--- src/pages/posts/posts.js
+++ src/pages/posts/posts.js
@@ -8,8 +8,9 @@ import React, { useState, useEffect } from "react";
  */
 import Layout from "../../components/Layout/Layout";
 import Hero from "../../components/Hero/Hero";
+import Button from "../../components/Button";
 import Preloader from "../../components/Preloader/Preloader";
-import { axiosInstance } from "../../_helpers/utils";
+import { axiosInstance, generateExcerpt, slugify } from "../../_helpers/utils";
 
 import styles from "./posts.module.scss";
 
@@ -35,7 +36,7 @@ const Posts = () => {
 	const fetchPosts = async () => {
 		await axiosInstance({
 			method: 'get',
-			url: `post`
+			url: `posts`
 		}).then(( res ) => {
 			setPosts( res.data );
 		}).catch( fetchPostsFail => {
@@ -86,7 +87,17 @@ const Posts = () => {
 							{ posts &&
 								posts.map( ( post ) => (
 									<div key={ post.id }>
-
+										<h3 className="wow fadeInUp" data-wow-delay=".3s">
+											{ post.title.rendered }
+										</h3>
+										<div 
+											className="wow fadeInUp" 
+											data-wow-delay=".5s"
+											dangerouslySetInnerHTML={{
+												__html: generateExcerpt( post.content.rendered, 0, 200 ) 
+											}}
+										/>
+										<Button variant="arrow" text="Read More" page={ `/news/${ slugify( post.title.rendered ) }/${ post.id }` } />
 									</div>
 								))
 							}
