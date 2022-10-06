3e3b6f6e69e9233ee60b0384be7e4dc178b6c0b1
Eugene Kyale <eugene.kyale@gmail.com>
2022-10-07 02:11:14 +0300

    push master

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,31 +1,39 @@
 /**
  * External Dependencies
  */
-import React from "react";
+ import React from "react";
 
-/**
- * Internal Dependencies
- */
-import Layout from "../../components/Layout";
-
-import "./404.scss";
-
-const Four0Four = () => {
-	return (
-		<Layout pageTitle="404">
-			<main className="four0four">
-				<div className="wow zoomIn" data-wow-delay=".3s">
-					<lottie-player
-						src="https://assets2.lottiefiles.com/packages/lf20_suhe7qtm.json"
-						background="transparent"
-						speed="1"
-						loop
-						autoplay
-					/>
-				</div>
-			</main>
-		</Layout>
-	);
-};
-
-export default Four0Four;
+ /**
+  * Internal Dependencies
+  */
+ import Layout from "../../components/Layout";
+ 
+ import "./404.scss";
+ 
+ const Four0Four = () => {
+	 return (
+		 <Layout pageTitle="404">
+			 <main className="four0four">
+				 <h3>
+					 The site is currently undergoing an update...
+				 </h3>
+				 <div className="four0four__inner wow zoomIn" data-wow-delay=".3s">
+					 <lottie-player
+						 src="https://assets2.lottiefiles.com/packages/lf20_suhe7qtm.json"
+						 background="transparent"
+						 speed="1"
+						 loop
+						 autoplay
+						 style={{
+							 width: 600,
+							 height: 'auto'
+						 }}
+					 />
+				 </div>
+			 </main>
+		 </Layout>
+	 );
+ };
+ 
+ export default Four0Four;
+ 
