b5a27cebceb2380fe0b4b7b235d4d77a1ecb256b
Eugene Kyale <eugene.kyale@gmail.com>
2022-10-07 01:33:12 +0300

    use https

--- src/_helpers/utils.js
+++ src/_helpers/utils.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export const toAbsoluteUrl = ( pathname ) => process.env.PUBLIC_URL + pathname;
 
-export const cmsUrl = "http://139.59.25.79/";
-
-export const baseUrl = "http://139.59.25.79/wp-json/wp/v2/";
+export const baseUrl = "https://139.59.25.79/wp-json/wp/v2/";
 
 /**
  * Convert any date into dd-mm-yyyy
