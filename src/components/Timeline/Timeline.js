a43c27071194d7d49d1e2e86d5bb3e419eb35ad5
Eugene Kyale <eugene.kyale@gmail.com>
2023-01-29 19:39:15 +0300

    initial tabs

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -2,18 +2,58 @@
  * External Dependencies
  */
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 /**
  * Internal Dependencies
  */
-import styles from "./Timeline.module.scss";
+import "./Timeline.scss";
 
 const Timeline = () => {
 
 	return (
-		<div className={ styles.timeline }>
-			
-		</div>
+		<Tabs>
+			<TabList>
+				<Tab>
+					1980's
+				</Tab>
+				<Tab>
+					1990's
+				</Tab>
+				<Tab>
+					1980's
+				</Tab>
+				<Tab>
+					1990's
+				</Tab>
+				<Tab>
+					1980's
+				</Tab>
+				<Tab>
+					1990's
+				</Tab>
+			</TabList>
+		
+			<TabPanel>
+				Moved to Ethiopia for trade in revamped second-hand coffee machines, cars, spare parts and blending machines.
+			</TabPanel>
+			<TabPanel>
+				After the deportation of the Eritreans from Ethiopia, Tekie and Ghebre formed ARTEGE with an Eritrea partner to trade in the coffee beans business, importing it from Kenya, Uganda and Tanzania.
+			</TabPanel>
+			<TabPanel>
+				Moved to Ethiopia for trade in revamped second-hand coffee machines, cars, spare parts and blending machines.
+			</TabPanel>
+			<TabPanel>
+				After the deportation of the Eritreans from Ethiopia, Tekie and Ghebre formed ARTEGE with an Eritrea partner to trade in the coffee beans business, importing it from Kenya, Uganda and Tanzania.
+			</TabPanel>
+			<TabPanel>
+				Moved to Ethiopia for trade in revamped second-hand coffee machines, cars, spare parts and blending machines.
+			</TabPanel>
+			<TabPanel>
+				After the deportation of the Eritreans from Ethiopia, Tekie and Ghebre formed ARTEGE with an Eritrea partner to trade in the coffee beans business, importing it from Kenya, Uganda and Tanzania.
+			</TabPanel>
+		</Tabs>
 	);
 };
 
